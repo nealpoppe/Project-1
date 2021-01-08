9fc54cb2f96202b447dd693c1a1e649a17bf8388
nealpoppe <npoppe@gmail.com>
2021-01-08 13:51:34 -0600

    readme and psudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("script working");
+
+/*
+create a 4x4 grid (16 objects)
+initially blue back
+will need 8 pictures for the pairs
+can hard code which picture goes where
+on click will reveal picture
+    need to store image on click
+    will need to compare after the second click
+can only click twice
+    if they match, they stay face up and the user can click again
+    if they do not match, they flip back down
+*/
