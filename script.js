2edf6b0f02a1dc29dc374260d3808a31116ee351
nealpoppe <npoppe@gmail.com>
2021-01-11 08:54:41 -0600

    click events

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("script working");
-
 /*
 create a 4x4 grid (16 objects)
 initially blue back
@@ -13,3 +11,58 @@ can only click twice
     if they do not match, they flip back down
 */
 
+// initial cards object
+// hard coded for now
+var cards = {
+    box01: {card: "A", faceUp: false},
+    box02: {card: "A", faceUp: false},
+    box03: {card: "B", faceUp: false},
+    box04: {card: "B", faceUp: false},
+    box05: {card: "C", faceUp: false},
+    box06: {card: "C", faceUp: false},
+    box07: {card: "D", faceUp: false},
+    box08: {card: "D", faceUp: false},
+    box09: {card: "E", faceUp: false},
+    box10: {card: "E", faceUp: false},
+    box11: {card: "F", faceUp: false},
+    box12: {card: "F", faceUp: false},
+    box13: {card: "G", faceUp: false},
+    box14: {card: "G", faceUp: false},
+    box15: {card: "H", faceUp: false},
+    box16: {card: "H", faceUp: false}
+}
+
+// variables to hold cards to compare
+let cardOne = "";
+let cardTwo = "";
+
+// setting up click event
+let currentCard = document.querySelectorAll(".cards");
+let currentTurn = document.querySelector(".playerPrompt");
+for(i=0; i<currentCard.length; i++) {
+    currentCard[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        // testing if first click
+        // need to change style on click to show the front of the card
+        // and the text from the object
+        if(currentTurn.innerHTML == "Please choose your first card") {
+            cardOne = e.target.id;
+            e.target.style.background = "white";
+            e.target.innerHTML = cards[cardOne].card;
+            currentTurn.innerHTML = "Please choose your second card"
+        }else if(currentTurn.innerHTML == "Please choose your second card") {
+            cardTwo = e.target.id;
+            e.target.style.background = "white";
+            e.target.innerHTML = cards[cardTwo].card;
+            currentTurn.innerHTML = "Please choose your first card"
+            // console.log(String(cardOne));
+            // console.log(cardTwo);
+            console.log(cards[cardOne]);
+        }
+    })
+}
+
+// function to check match
+isMatch(){
+    
+}
